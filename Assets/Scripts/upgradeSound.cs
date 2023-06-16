dac2b327d6f2394d057c0e7f5273133ffa0ae7f0
JoaoVitorMenezesMagalhaes <joaovmm@al.insper.edu.br>
2023-06-16 00:55:09 -0300

    tah

--- Assets/Scripts/upgradeSound.cs
+++ Assets/Scripts/upgradeSound.cs
@@ -3,34 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class upgradeSound : MonoBehaviour
 {
     public AudioSource audio1;
-    public AudioSource audio2;
+    //public AudioSource audio2;
     private bool isSoundPlaying = false;
+
+    private void Awake()
+    {
+        audio1.Stop();
+        //audio2.Stop();
+        isSoundPlaying = false;
+    }
+
+    private void OnEnable()
+    {
+        audio1.Stop();
+        //audio2.Stop();
+        isSoundPlaying = false;
+    }
 
     public void playUpgrade()
     {
         if (!isSoundPlaying)
         {
-            int rand = Random.Range(0, 100);
-            if (rand < 50){
-                audio1.Play();
-                isSoundPlaying = true;
-                StartCoroutine(WaitForSoundToEnd());
-            }
-            else{
-                audio2.Play();
-                isSoundPlaying = true;
-                StartCoroutine(WaitForSoundToEnd());
-            }
-                
+            audio1.Play();
+            isSoundPlaying = true;
+            StartCoroutine(WaitForSoundToEnd());
         }
     }
 
     IEnumerator WaitForSoundToEnd()
     {
-        while (audio1.isPlaying || audio2.isPlaying)
+        while (audio1.isPlaying)
         {
             yield return null;
         }
